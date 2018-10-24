2694f71fa7ea4e7c9db307e301403036a3ef4d25
liucaifengname <1213637440@qq.com>
2018-10-24 17:43:26 +0800

    first commit

--- testsuites/TestRunner2.py
+++ testsuites/TestRunner2.py
@@ -3,19 +3,18 @@ import os
 import unittest
 import time
 
-
+test_dir = './'
+suite=unittest.TestLoader().discover(test_dir,pattern='test*.py')
 report_path=os.path.dirname(os.path.abspath('.'))+'./test_report/'
-now=time.strftime('%Y%m%d%H%M',time.localtime(time.time()))
-HtmlFile=report_path + now + '_result.html'
-# HtmlFile=report_path + '_result.html'
-
-
+HtmlFile=report_path  + '_result.html'
 fp=open(HtmlFile,'wb')
-
-suite=unittest.TestLoader().discover('testsuites',pattern='test*.py')
 
 if __name__ == '__main__':
 
-    runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'WEB UI 自动化测试报告，测试结果如下',)
+    runner=HTMLTestRunner.HTMLTestRunner(stream=fp,title=u'WEB UI 自动化测试报告，测试结果如下',description=u"用例测试情况如下")
     runner.run(suite)
     fp.close()
+
+
+
+
